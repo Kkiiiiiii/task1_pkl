cd236ccd8cfc5038892fc0840753e3f62354753e
Kkiiiiiii <rikizhakaria@gmail.com>
2025-12-22 14:42:22 +0700

    layout

---
+++ app/Http/Controllers/EducateController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Educate;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EducateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Educate $educate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Educate $educate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Educate $educate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Educate $educate)
+    {
+        //
+    }
+}
