a5f97c0b64d33ca9c9756f3eef113168d3a29786
Kkiiiiiii <rikizhakaria@gmail.com>
2025-12-23 15:23:45 +0700

    login n regis

--- resources/views/karyawan/auth/login.blade.php
+++ resources/views/karyawan/auth/login.blade.php
@@ -71,13 +71,16 @@
                   </div>
               @endif
 
-              {{-- Error message --}}
-              @if(session('error'))
-                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                      {{ session('error') }}
-                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>
-    @endif
+          @if ($errors->any())
+         <div class="alert alert-danger">
+             <ul>
+                 @foreach ($errors->all() as $error)
+                     <li>{{ $error }}</li>
+                 @endforeach
+             </ul>
+         </div>
+     @endif
+
     <div class="container-xxl">
       <div class="authentication-wrapper authentication-basic container-p-y">
         <div class="authentication-inner py-6">
@@ -128,8 +131,8 @@
                     type="text"
                     class="form-control"
                     id="email"
-                    name="email-username"
-                    placeholder="Enter your email or username"
+                    name="email"
+                    placeholder="Enter your email"
                     autofocus />
                 </div>
                 <div class="mb-6 form-password-toggle">
--- resources/views/karyawan/auth/register.blade.php
+++ resources/views/karyawan/auth/register.blade.php
@@ -58,6 +58,15 @@
 </head>
 
 <body>
+     @if ($errors->any())
+         <div class="alert alert-danger">
+             <ul>
+                 @foreach ($errors->all() as $error)
+                     <li>{{ $error }}</li>
+                 @endforeach
+             </ul>
+         </div>
+     @endif
     <!-- Content -->
     <div class="container-xxl">
         <div class="authentication-wrapper authentication-basic container-p-y">
